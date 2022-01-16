2d20f5424c015eeb5929df492542328eaa6332d7
daad1 <daadrasheid@gmail.com>
2022-01-16 22:14:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationdemo/Fragment_home.kt
@@ -0,0 +1,33 @@
+package com.example.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.NavInflater
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class Fragment_home : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<TextView>(R.id.tvHome).setOnClickListener {
+           Navigation.findNavController(view).navigate(R.id.action_fragment_home_to_fragment_new)
+        }
+        return view
+    }
+
+    }
---
+++ app/src/main/java/com/example/navigationdemo/Fragment_new.kt
@@ -0,0 +1,33 @@
+package com.example.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class Fragment_new : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_new, container, false)
+        view.findViewById<TextView>(R.id.tvNew).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragment_new_to_fragment_home)
+        }
+
+        return view
+    }
+
+
+}
